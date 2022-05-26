3f58af03e739568cec0142a4601aa23f0eb20778
Michelangelo Basso <michelangelobasso.dev@gmail.com>
2022-05-26 22:41:46 +0200

    feat(Config): :clown_face: Add oak.config.js

--- templates/oak.config.js
+++ templates/oak.config.js
@@ -1,22 +1,24 @@
 export default {
     question: 'In which project you need to generate stuff',
     trees: {
-        name: 'project1',
-        path: 'path/to/the/project', // must end with no slash...
-        nodes: {
-            node_1: {
-                name: 'node 1',
-                folder: 'node-1-folder',
-                leafs: [
-                    {
-                        name: 'leaf1-of-node-1',
-                        script: 'echo hello',
-                        folder: 'leaf-folder',
-                        options: '--project=something' // options gets appended after the name or path
-                    }
-                ],
+        tree_1: {
+            name: 'project1',
+            path: 'path/to/the/project', // must end with no slash...
+            nodes: {
+                node_1: {
+                    name: 'node 1',
+                    folder: 'node-1-folder',
+                    leafs: [
+                        {
+                            name: 'leaf1-of-node-1',
+                            script: 'echo hello',
+                            folder: 'leaf-folder',
+                            options: '--project=something' // options gets appended after the name or path
+                        }
+                    ],
+                }
             }
-        }
-    },
+        },
+    }
 };
   
