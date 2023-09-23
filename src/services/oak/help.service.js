4197b0015ed37f06f38510a97589a4d2c7419ca6
Niskola <83880514+CamelTrue@users.noreply.github.com>
2023-09-23 19:35:40 +0200

    I moved the SC and PV services to help.service.js

--- src/services/oak/help.service.js
+++ src/services/oak/help.service.js
@@ -1,4 +1,11 @@
-// TODO MOVE DOCS AND INFO IN help.service.js
+import chalk from 'chalk';
+import cfonts from 'cfonts';
+import table from 'text-table';
+import { exec } from 'child_process';
+import { config, package_json } from './core.service.js';
+
+
+
 /** 
  * =====================
  * DOCUMENTATION & INFOS
@@ -10,7 +17,8 @@
  * @param {string} topic the topic to see the documentation from
  * @returns void
  */
- const openDocumentation = (topic) => {
+
+const openDocumentation = (topic) => {
     const documentation = DOCS[topic];
 
     /**
@@ -44,4 +52,123 @@
     process.exit(1);
 };
 
-export default openDocumentation
+
+
+/**
+ * Gets a cool ASCII name and achronym of the oak CLI. Also it prints the 
+ * version and the npm packages that have been used.
+ * @returns void
+ */
+const promptVersion = () => {
+    const oak = config.name.toUpperCase();
+    const oak_name = `${oak} CLI`;
+    const cfonts_config = {
+        font: 'block',              // define the font face
+        align: 'left',              // define text alignment
+        colors: ['green'],         // define all colors
+        background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+        letterSpacing: 1,           // define letter spacing
+        lineHeight: 1,              // define the line height
+        space: true,                // define if the output text should have empty lines on top and on the bottom
+        maxLength: '0',             // define how many character can be on one line
+        gradient: false,            // define your two gradient colors
+        independentGradient: false, // define if you want to recalculate the gradient for each new line
+        transitionGradient: false,  // define if this is a transition between colors directly
+        env: 'node'  
+    };
+
+    //ASCII
+    const oak_ascii = cfonts.render(oak_name, cfonts_config);
+
+    /**
+     * Prints name, achronym, version and description
+     */
+
+    console.log(oak_ascii.string);
+    console.log('-----------------------------------------------------\n');
+
+    console.log(`${oak} CLI: ${chalk.hex(config.prompts.success.color)(package_json.version)}`);
+    console.log(`${package_json.description}\n`);
+
+    /**
+     * Builds the npm packages table
+     */
+    //TABLE
+    //HEAD
+    const TAB = '      ';
+    const table_heads = table([
+        ['Packages', `${TAB}  Version`]
+    ]);
+    console.log(table_heads);
+    console.log('-----------------------------------------------------');
+
+    //ROWS
+    let table_rows = [];
+    Object.keys(package_json.dependencies).forEach((name) => {
+        table_rows.push([name, `${TAB}${package_json.dependencies[name].substring(1)}`]);
+    });
+
+    console.log(table(table_rows));
+    console.log('\n');
+    process.exit(1);
+};
+
+
+/**
+ * Prints all the commands options
+ * @returns void
+ */
+const showCommands = () => {
+
+    // CONSTANTS
+    const options = config.options;
+    const args = config.args;
+
+    // VARIABLES
+    let options_rows = [];
+    let args_rows = [];
+
+
+    /**
+     * Options table build
+     */
+    Object.keys(options).forEach((option) => {
+        /**
+         * Here we build the commands from the options
+         */
+        const command = chalk.hex(PROMPTS.info.color).bold(options[option].cmd);
+        const option_description = `(${options[option].name}) ${options[option].description}`;
+
+        options_rows.push([command, option_description]);
+    });
+
+    /**
+     * Args table build
+     */
+    const args_keys = Object.keys(args);
+
+    args_keys.forEach((arg) => {
+        args_rows.push([chalk.hex(config.prompts.info2.color).bold(args[arg].cmd), `(${args[arg].name}) ${args[arg].description}`]);
+    });
+
+    /**
+     * Options table prompt
+     */
+    console.log(`\n${chalk.hex(config.prompts.info.color).inverse('OPTIONS')}`);
+    console.log(table(options_rows));
+
+    /**
+     * Args table prompt
+     */
+    console.log(`\n${chalk.hex(config.prompts.info2.color).inverse('ARGS')}`);
+    console.log(table(args_rows));
+
+    process.exit(1);
+};
+
+
+export default { 
+    openDocumentation, 
+    promptVersion, 
+    showCommands 
+};
